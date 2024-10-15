71b0b7416f67398f5d18809a4bd66013bc878868
Aurélien HAYE <aurelien.haye.techdeco@gmail.com>
2024-10-15 11:52:50 +0200

    update with figlet

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 import chalk from 'chalk';
+import figlet from 'figlet';
 
 const log = console.log;
 
 log(chalk.blue('Benjamin')," "+ chalk.red('Damien')," "+ chalk.green('Rémi')," "+ chalk.yellow('Marco'));
+
+figlet("Hello World!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
